510cdf026ceb8ea396d500a8ecfc8a8b654fc19a
Shashwat-CODING <bob17040246@gmail.com>
2025-03-01 21:44:34 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,30 +1,11 @@
 const express = require("express");
 const ytdl = require("@distube/ytdl-core");
 const cors = require("cors");
-const puppeteer = require("puppeteer-core");
-const chromium = require("chromium");
 
 const app = express();
 const PORT = 3100;
 
 app.use(cors());
-
-// Function to get YouTube cookies using Puppeteer
-async function getYouTubeCookies() {
-    const browser = await puppeteer.launch({
-        executablePath: chromium.path,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        headless: true
-    });
-
-    const page = await browser.newPage();
-    await page.goto('https://www.youtube.com');
-
-    const cookies = await page.cookies();
-    await browser.close();
-
-    return cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; ');
-}
 
 app.get("/streams/:videoId", async (req, res) => {
     const videoId = req.params.videoId;
@@ -35,12 +16,7 @@ app.get("/streams/:videoId", async (req, res) => {
 
     try {
         const url = `https://www.youtube.com/watch?v=${videoId}`;
-        const cookies = await getYouTubeCookies();
-
-        const info = await ytdl.getInfo(url, {
-            requestOptions: { headers: { Cookie: cookies } }
-        });
-
+        const info = await ytdl.getInfo(url);
         const audioFormats = ytdl.filterFormats(info.formats, "audioonly");
 
         if (audioFormats.length > 0) {
